43921bdfeac2e750f73cfe698f910e22d29445ad
Brett Baker <gitbabb@mybabb.com>
2026-07-15 05:51:53 -0700

    making a little landing page

---
+++ src/Pages/CoverPageFolder/zCover.jsx
@@ -0,0 +1,26 @@
+import "./Cover.css"
+ 
+const AFrontPage = () => {
+  return (
+    <div className="top-0 left-0 fixed w-screen h-full  bg-[#111422]">
+       <div className="relative flex justify-center items-center w-full h-full ">
+
+     
+
+        <p className="underConstruction ">
+             Patience Please
+           <br />
+             I am Building
+             <br />
+             A New Super Cool 
+             <br />
+             Landing Page
+             
+        </p>
+     
+        </div>  
+    </div>
+  )
+}
+
+export default AFrontPage
