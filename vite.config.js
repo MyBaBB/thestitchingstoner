1558e8dd5d07382e078da0d41e32972b14c6e4a5
Brett Baker <gitbabb@mybabb.com>
2026-07-16 03:02:12 -0700

    update to swc vite update,  save as boilerplate.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   assetsInclude: ['**/*.glb'],
   plugins: [react()],
 })
+
