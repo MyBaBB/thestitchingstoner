f89f3174334099d6f99f5ada5b88da45560ed334
Brett Baker <gitbabb@mybabb.com>
2026-07-18 07:41:38 -0700

    made my button less descrete

--- src/Components/ContactMeFolder/ContactMe.jsx
+++ src/Components/ContactMeFolder/ContactMe.jsx
@@ -1,28 +1,29 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { PiTree } from "react-icons/pi";
+import { GiBat } from "react-icons/gi";
 import "./ContactMe.css";
 
 const ContactMe = () => {
   return (
-    <div className="ContactButtonHide relative z-50 m-auto mb-[.5rem] flex h-full w-full items-center justify-center">
+    <div className="ContactButtonHide relative z-50 m-auto mb-[.5rem] flex h-full w-full 
+                    items-center justify-center
+               ">
       <div
         id="contact"
-        className="buttonsAllWrapper m-auto mb-4 w-fit px-1 pb-2 cursor-pointer duration-500 ease-in-out hover:scale-[100.8%]"
+        className="buttonsAllWrapper m-auto mb-4 w-fit px-1 pb-2 cursor-pointer 
+                   duration-500 ease-in-out hover:scale-[100.8%]"
       >
         <a href="https://mybabb.com "  className=" relative flex-row justify-center  
                  ">
-         <button
+<button
   className="rootsButton w-fit whitespace-nowrap font-extrabold 
    "
   title="Contact Me"
 >
   <div className="piTree flex items-center justify-center ">
-    <PiTree size={20} />
+   <GiBat size={30} />
   </div>
 
-  <span className="rootsText font-Itim-Regular">Roots</span>
-   <hr className="relative flex rootsLine  h-[1px] w-10 bg-transparent border-[.2px] rounded-lg" />
 </button>
 
          
