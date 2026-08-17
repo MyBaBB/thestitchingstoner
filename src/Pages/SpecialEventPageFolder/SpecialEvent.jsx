242d5c8b9aad2697f55d39f580409132e7e06f3f
Brett Baker <gitbabb@mybabb.com>
2026-08-17 04:10:36 -0700

    break time

--- src/Pages/SpecialEventPageFolder/SpecialEvent.jsx
+++ src/Pages/SpecialEventPageFolder/SpecialEvent.jsx
@@ -5,12 +5,12 @@ import Footer from "../../Components/FooterFolder/Footer.jsx";
 import YouTubePlayer from "../../Components/YouTubeFolder/YouTubePlayer.jsx";
 import "./SpecialEvent.css";
 import VanSlideIn from "../../Components/VanSlideInFolder/VanSlideIn.jsx";
-import SuperBG from "../../Components/Super-Sample-BG-Folder/Super-Sample-BG-1.jsx";
+ 
 
 export default function SpecialEventPage() {
   return (
     <>
-  <SuperBG />
+ 
 
   <div className="page-content relative z-10 min-h-screen w-full overflow-x-hidden overflow-y-auto">
 
@@ -28,11 +28,13 @@ export default function SpecialEventPage() {
 
         <YouTubePlayer />
       </div>
+       <Footer />
     </div>
+   
   </div>
 
   {/* FOOTER MOVED OUTSIDE */}
-  <Footer />
+  
 </>
 
   );
