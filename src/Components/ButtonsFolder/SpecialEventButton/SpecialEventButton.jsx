f69b8ec70800848c31601cec4b9947edd3415a74
Brett Baker <gitbabb@mybabb.com>
2026-08-17 01:07:00 -0700

    anchor tag wasnt recognized in render so replaced with link to

--- src/Components/ButtonsFolder/SpecialEventButton/SpecialEventButton.jsx
+++ src/Components/ButtonsFolder/SpecialEventButton/SpecialEventButton.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./SpecialEventButton.css";
 
 const SpecialEventButton = () => {
+  
   return (
     <Link to = "/specialevent"
       className="special-event-btn"
