38ff96828e833df210e7749427754ee54d2f3e1a
Brett Baker <gitbabb@mybabb.com>
2026-08-23 06:29:34 -0700

    ok to test layout the do the images

--- src/Pages/SpecialEventPageFolder/SpecialEvent.jsx
+++ src/Pages/SpecialEventPageFolder/SpecialEvent.jsx
@@ -1,41 +1,35 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from "react";
+// SpecialEventPage.jsx
+import React from "react";
 import ReverseButton from "../../Components/ButtonsFolder/ReverseButton/ReverseButton.jsx";
 import Footer from "../../Components/FooterFolder/Footer.jsx";
 import YouTubePlayer from "../../Components/YouTubeFolder/YouTubePlayer.jsx";
 import AnimatedDetails from "../../Components/AnimatedDetailsFolder/AnimatedDetails.jsx";
-import "./SpecialEvent.css";
 import VanSlideIn from "../../Components/VanSlideInFolder/VanSlideIn.jsx";
-
 import Copyright from "../../Components/CopyrightFolder/Copyright.jsx";
+import "./SpecialEvent.css";
 
 export default function SpecialEventPage() {
   return (
-    <>
-  <div className="page-content ">
+    <div className="special-event-bg-wrapper">
+      <div className="page-content">
         <ReverseButton />
 
         <div className="specialEventPage">
-          <div className="specialEventContent relative flex flex-col items-center  ">
+          <div className="specialEventContent relative flex flex-col items-center">
             <VanSlideIn />
-               
-
-
-            < AnimatedDetails />
+            <AnimatedDetails />
             <YouTubePlayer />
           </div>
 
-           {/* Wrap anchor inline-block with w-fit to prevent layout stretching */}
-      <a href="https://mybabb.com/youtubepageserious" className="inline-block w-auto">
+          <a href="https://mybabb.com/youtubepageserious" className="inline-block w-auto">
             <div className="tooltip-wrapper1">
-                <Copyright />
+              <Copyright />
             </div>
-      </a>
+          </a>
+
           <Footer />
         </div>
-  </div>
-
-     
-    </>
+      </div>
+    </div>
   );
 }
