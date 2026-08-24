1d3f04e8a97ce4520637746028acf7ee8325b98b
Brett Baker <gitbabb@mybabb.com>
2026-08-24 09:41:56 -0700

    made the robot go away after a few seconds and add a link to where to find it on the more descrete copyright button

--- src/Pages/SpecialEventPageFolder/SpecialEvent.jsx
+++ src/Pages/SpecialEventPageFolder/SpecialEvent.jsx
@@ -1,6 +1,5 @@
- 
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReverseButton from "../../Components/ButtonsFolder/ReverseButton/ReverseButton.jsx";
 import HippieFootprints from "../../Components/HippieFootprintsFolder/HippieFootprints.jsx";
 import NotRobot from "../../Components/NotRobotFolder/NotRobot.jsx"; 
@@ -10,60 +9,59 @@ import { Link } from "react-router-dom";
 import VanSlideIn from "../../Components/VanSlideInFolder/VanSlideIn.jsx";
 import Copyright from "../../Components/CopyrightFolder/Copyright.jsx";
 import "./SpecialEvent.css";
-import "../../Components/AnimatedDetailsFolder/AnimatedDetails.jsx";
 
 export default function SpecialEventPage() {
+  const [showRobot, setShowRobot] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowRobot(false);
+    }, 14000); //  seconds
+
+    return () => clearTimeout(timer); // Clean up timer on unmount
+  }, []);
+
   return (
-<>
- 
-    <div className="special-event-bg-wrapper ">
-    < HippieFootprints />
-  <div className="three-column-layout">
+    <>
+      <div className="special-event-bg-wrapper ">
+        <HippieFootprints />
+        <div className="three-column-layout">
 
-    {/* LEFT COLUMN */}
-  <div className="side-column left-column    ">
-  
-     <div className="absolute top-4 left-4" > <ReverseButton />
-     </div>
-     
-     <div className=" absolute bottom-4 left-4">
-       <Copyright />
-     </div>
-  
-  </div>
+          {/* LEFT COLUMN */}
+          <div className="side-column left-column">
+            <div className="absolute top-4 left-4">
+              <ReverseButton />
+            </div>
+            <a href="https://mybabb.com" className="absolute bottom-4 left-4">
+              <Copyright />
+            </a>
+          </div>
 
-    {/* MIDDLE COLUMN */}
-    <div className="middle-content  ">
-      <div className="specialEventContent relative flex flex-col items-center">
-        <VanSlideIn />
-        <AnimatedDetails />
-        <YouTubePlayer />
-      </div>
+          {/* MIDDLE COLUMN */}
+          <div className="middle-content">
+            <div className="specialEventContent relative flex flex-col items-center">
+              <VanSlideIn />
+              <AnimatedDetails />
+              <YouTubePlayer />
+            </div>
+          </div>
 
-      
-    </div>
+          {/* RIGHT COLUMN */}
+          <div className="side-column right-column">
+            {showRobot && (
+              <Link to="/localartist" className="absolute right-4 bottom-4">
+                <div
+                  className="footer-right_Tooltip"
+                  data-tool-tip=" 🌈 This Website Chooses Real Art 🎨 "
+                >
+                  <NotRobot />
+                </div>
+              </Link>
+            )}
+          </div>
 
-    {/* RIGHT COLUMN */}
-    <div className="side-column right-column   ">
-      
-      <Link to="/localartist" className="absolute right-4 bottom-4">
-        <div className="footer-right_Tooltip"
-         data-tool-tip=" 🌈 This Website Chooses Real Art 🎨 ">
-                 
-            < NotRobot />
         </div>
-      </Link>
-    </div>
-
-  </div>
-
-</div>
-
-    
- 
-  
- 
- 
-</>
+      </div>
+    </>
   );
 }
