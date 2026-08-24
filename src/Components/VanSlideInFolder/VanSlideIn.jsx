1a40cec7d43c8b98c8e5f88d364516554c225bac
Brett Baker <gitbabb@mybabb.com>
2026-08-24 08:40:33 -0700

    test all this ,  add more image for large screen next on the two 33 pecent columns created

--- src/Components/VanSlideInFolder/VanSlideIn.jsx
+++ src/Components/VanSlideInFolder/VanSlideIn.jsx
@@ -4,12 +4,12 @@ const VanSlideIn = () => {
   return (
     <div>
       <a href="https://thestitchingstoner.square.site/product/crochet-101-the-basics/FR5XDEGDJ3HEPOUPPTVS2BNP">
-        <h1 className="specialEventTitle">
-          🌈 Hookahville 62 ✌🏼
+        <h1 className="specialEventTitle text-center">
+          <span className="hookahMobile ">🌈 Hookahville 62 ✌🏼</span>
           <br />
           <span className="vanBounce scale-x-[-1]">🚐</span>
           &nbsp;
-          <span className="sessionBump">
+          <span className="sessionBump  ">
             <span className="crochetRotate">Yarn</span> Sessions
           </span>
           <span className="yarnWiggle">🧶</span>
