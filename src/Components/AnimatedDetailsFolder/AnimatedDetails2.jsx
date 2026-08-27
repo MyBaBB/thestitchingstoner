e4db130736aed99d8093e28e91962364f12efbd5
Brett Baker <gitbabb@mybabb.com>
2026-08-27 04:03:37 -0700

    test mobile

--- src/Components/AnimatedDetailsFolder/AnimatedDetails2.jsx
+++ src/Components/AnimatedDetailsFolder/AnimatedDetails2.jsx
@@ -7,7 +7,7 @@ import img22 from "./Zombies5-600x400.webp";
 import img33 from "./Zombies-4600x400.webp";
 import img44 from "./Zombies3-600x400.webp";
 import img55 from "./Zombies2-600x400.webp";
-
+import VanSlideIn2 from "../../Components/VanSlideInFolder/VanSlideIn2.jsx";
 const IMAGES = [img11, img22, img33, img44, img55];
 
 const AnimatedDetails = () => {
@@ -29,6 +29,7 @@ const AnimatedDetails = () => {
   }, []);
 
   return (
+    <>
     <div className="trippy-stage2 ">
       <div className="trippy-card2 flex flex-col items-center text-center font-Itim-Regular leading-relaxed  ">
 
@@ -43,7 +44,10 @@ const AnimatedDetails = () => {
           )}
         </div>
      
-     
+       <a href="https://thestitchingstoner.square.site/product/crochet-101-the-basics/FR5XDEGDJ3HEPOUPPTVS2BNP" className="hover:opacity-90 hover:scale-105 ">
+       < VanSlideIn2 />
+       </a>
+
      < div className="stonerText  border-4 border-blue-500 ">
             <p className="stonerLine2   line11 " data-text="High‑vibe Stitching for Classes">
               <span className="inline-block whitespace-nowrap z-1">
@@ -83,6 +87,7 @@ const AnimatedDetails = () => {
        </div>
       </div>
     </div>
+    </>
   );
 };
 
