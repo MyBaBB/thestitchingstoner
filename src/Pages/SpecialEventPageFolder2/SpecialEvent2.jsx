3afe762a7ca7cfd89dbdebbc3448f0241c0e6373
Brett Baker <gitbabb@mybabb.com>
2026-09-06 09:03:27 -0700

    fixed - goody - sm image

--- src/Pages/SpecialEventPageFolder2/SpecialEvent2.jsx
+++ src/Pages/SpecialEventPageFolder2/SpecialEvent2.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import ReverseButton from "../../Components/ButtonsFolder/ReverseButton/ReverseButton.jsx";
 import HippieFootprints from "../../Components/HippieFootprintsFolder/HippieFootprints.jsx";
- import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AnimatedDetails2 from "../../Components/AnimatedDetailsFolder/AnimatedDetails2.jsx";
 import StonerLogo from "../../Images/300x300-r.webp";
 import Copyright from "../../Components/CopyrightFolder/Copyright.jsx";
@@ -26,28 +26,30 @@ export default function SpecialEvent2() {
         <div className="three-column-layout2">
           {/* LEFT COLUMN */}
           <div className="side-column2 left-column2">
-            <div className="absolute left-4 top-4">
+            <div className="absolute left-4 top-4 z-20">
               <ReverseButton />
             </div>
-            <a href="https://mybabb.com" className="absolute bottom-4 left-4">
+            <a href="https://mybabb.com" className="absolute bottom-4 left-4 z-20">
               <Copyright />
             </a>
           </div>
 
           {/* MIDDLE COLUMN */}
           <div className="middle-content2">
-            <div className="specialEvent2Content relative flex flex-col items-center text-white">
+            <div className="specialEvent2Content w-full flex flex-col items-center text-white">
               <AnimatedDetails2 />
             </div>
           </div>
 
-          <div className="side-column2 right-column2 ">
-            {/*Enter RIGHT COLUMN Stuff*/}
-               <Link to="/coverpage" className="absolute top-4 right-4 z-10" >
-                <img src={StonerLogo}
-                 alt="" className="h-[120px] w-[120px] " />
-               </Link>
-
+          {/* RIGHT COLUMN */}
+          <div className="side-column2 right-column2">
+            <Link to="/coverpage" className="absolute top-4 right-4 z-20">
+              <img 
+                src={StonerLogo} 
+                alt="Stoner Logo" 
+                className="h-[120px] w-[120px] object-contain" 
+              />
+            </Link>
           </div>
         </div>
       </div>
